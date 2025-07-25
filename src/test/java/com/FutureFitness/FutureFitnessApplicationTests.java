f610a26a7c3be600aae4106309cfa204c0691f60
coder-jeshwanth <24mcaa32@kristujayanti.com>
2025-07-26 01:47:13 +0530

    Empty project created

---
+++ src/test/java/com/FutureFitness/FutureFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FutureFitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
